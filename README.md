31788641f011dc3b3b5ebf077ce365ff291b2727
Franck Ledoux <franck.ledoux@live.fr>
2022-04-15 19:04:54 +0200

    Update with a simplified intern gmds

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # concurrent_triangulation
 C++ Implementation of a concurrent triangulation algorithm based on gmds and std::thread
+
+
+## Compilation of the project
+We integrate a simple versio of gmds into this repository. Gmds contains the mesh data structure that we are going to 
+use.
+
+### Preparation of gmds
+1. Get into the extern repo 
+2. Extract the gmds archive with ``tar xzvf gmds.tgz`
+3. Compile gmds with the following set of commands in the extern repo (or anywhere you prefer)
+```bash
+cmake  gmds -B build_lib -DCMAKE_INSTALL_PREFIX=local_install 
+cmake --build build_lib --target install  
+```
+### Compilation and run of the current project
+Now the library gmds is installed where you expected. it can be 
+used to compile the current project. I use CLION for that but any other IDE can be used. Compilation is ensured with 
+cmake. You just have ot give the absolute path to the gmds install directory with the `CMAKE_PREFIX_PATH` command. For instance
+```
+-DCMAKE_PREFIX_PATH=/Users/franck/CLionProjects/concurrent_triangulation/extern/local_install
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,47 @@
+//---------------------------------------------------------------------------
 #include <iostream>
 #include <thread>
+#include <gmds/ig/Mesh.h>
+//---------------------------------------------------------------------------
+using namespace gmds;
+
+//---------------------------------------------------------------------------
+bool isInCircle(const math::Point& APoint, const Face& AFace) {
+    std::vector<Node> nodes = AFace.get<Node>();
+    math::Point p0 = nodes[0].point();
+    math::Point p1 = nodes[1].point();
+    math::Point p2 = nodes[2].point();
+    return false;
+}
+//---------------------------------------------------------------------------
+void initBoxMesh(Mesh* AMesh, const double AXMax, const double AYMax){
+    Node n0 = AMesh->newNode(0,0);
+    Node n1 = AMesh->newNode(AXMax,0);
+    Node n2 = AMesh->newNode(AXMax,AYMax);
+    Node n3 = AMesh->newNode(0,AYMax);
+    Face f1 = AMesh->newTriangle(n0,n1,n2);
+    Face f2 = AMesh->newTriangle(n2,n0,n3);
+    //update the connectivity N --> F
+    n0.add(f1);
+    n1.add(f1);
+    n2.add(f1);
+
+    n2.add(f2);
+    n0.add(f2);
+    n3.add(f2);
+
+}
+//---------------------------------------------------------------------------
 int main(){
     std::cout<<"Concurrent triangulation with gmds and std::thread"<<std::endl;
+    double x_max=10;
+    double y_max=10;
+
+    Mesh m(MeshModel(DIM3|N|F|F2N|N2F));
+
+    initBoxMesh(&m, x_max,y_max);
+    std::cout<<"The mesh has "<<m.getNbFaces()<<std::endl;
     std::thread t;
     return 0;
 }
+//---------------------------------------------------------------------------
